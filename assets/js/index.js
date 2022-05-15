697d8f3d3c7bb9eee95c86cb182b0618a0439e09
AliAhmed8585 <aliahmed6633366@gmail.com>
2022-05-15 15:05:00 +0200

    first commit.

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+// let x = $(".main").offset().top;
+// $(window).scroll(function()
+// {
+//     let wScorll = $(window).scrollTop();
+//     if (wScorll > x)
+//     {
+//         $(".nav-menu").css("backgroundColor","red")
+//         $(".nav-menu").css("padding","10px 50px")
+//         $(".btnUp").fadeIn(500);
+//     }
+//     else
+//     {
+//         $(".nav-menu").css("backgroundColor", "transparent")
+//         $(".nav-menu").css("padding","0px 0px")
+//         $(".btnUp").fadeOut(500);
+        
+//     }
+// })
+
+
+
+
+
